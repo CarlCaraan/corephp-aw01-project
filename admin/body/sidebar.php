ad8091fd6a2914b5517dfff10bfdf2d03bd6aedb
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-04-21 22:13:15 +0800

    4/21/22

---
+++ admin/body/sidebar.php
@@ -0,0 +1,50 @@
+    <aside id="left-panel" class="left-panel">
+        <nav class="navbar navbar-expand-sm navbar-default">
+
+            <div class="navbar-header">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="./"><img src="images/logo.png" alt="Logo"></a>
+                <a class="navbar-brand hidden" href="./"><img src="images/logo2.png" alt="Logo"></a>
+            </div>
+
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="<?php if (($toggle_active) == "dashboard") {
+                                    echo "active";
+                                } ?>">
+                        <a href="../admin/dashboard.php"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
+                    </li>
+                    <li class="menu-item-has-children dropdown <?php echo $toggle ?> <?php if ($toggle_active == "customer" || $toggle_active == "atm" || $toggle_active == "sps" || $toggle_active == "spsv1") { echo "active";
+                                                                                        }  ?>">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-user"></i>Customer</a>
+                        <ul class="sub-menu children dropdown-menu <?php echo $toggle ?>">
+                            <li style="<?php if (($toggle_active) == "customer") {
+                                            echo "font-weight: bold;";
+                                        } ?>"><i class="fa fa-credit-card"></i><a href="add_costumer.php">Add Costumer</a></li>
+                            <li style="<?php if (($toggle_active) == "atm") {
+                                            echo "font-weight: bold;";
+                                        } ?>"><i class="fa fa-credit-card"></i><a href="atm.php">ATM</a></li>
+                            <li style="<?php if (($toggle_active) == "sps") {
+                                            echo "font-weight: bold;";
+                                        } ?>"><i class="fa fa-money"></i><a href="sps.php">SPS</a></li>
+                            <li style="<?php if (($toggle_active) == "spsv1") {
+                                            echo "font-weight: bold;";
+                                        } ?>"><i class="fa fa-bars"></i><a href="spsv1.php">SPSV1</a></li>
+                        </ul>
+                    </li>
+                    <li class="<?php if (($toggle_active) == "chat") {
+                                    echo "active";
+                                } ?>">
+                        <a href="chat.php"> <i class="menu-icon fa fa-envelope"></i>Chat</a>
+                    </li>
+                    <li class="<?php if (($toggle_active) == "account") {
+                                    echo "active";
+                                } ?>">
+                        <a href="account.php"> <i class="menu-icon fa fa-users"></i>Account</a>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+    </aside><!-- /#left-panel -->
