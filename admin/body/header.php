627726719e75864f88885944c5cd0820f9316c17
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-04-24 18:53:22 +0800

    4/24/22

--- admin/body/header.php
+++ admin/body/header.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['username'])) {
     $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
     $user = mysqli_fetch_array($user_details_query);
 } else {
-    header("Location: ../login.php");
+    header("Location: ../login");
 }
 ?>
 
