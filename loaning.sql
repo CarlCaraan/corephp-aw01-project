d92c06c926e0a1535d53156f0cec8fbb38117105
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-05-03 18:53:51 +0800

    5/3/22

--- loaning.sql
+++ loaning.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2022 at 02:21 PM
+-- Generation Time: May 03, 2022 at 12:53 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -42,7 +42,9 @@ CREATE TABLE `chat` (
 
 INSERT INTO `chat` (`chat_message_id`, `to_user_id`, `from_user_id`, `chat_message`, `timestamp`, `status`) VALUES
 (22, 83, 68, 'test', '2022-05-01 12:38:00', 1),
-(23, 83, 84, 'I have a concern with my account', '2022-05-01 14:27:33', 1);
+(23, 83, 84, 'I have a concern with my account', '2022-05-01 14:27:33', 1),
+(25, 84, 68, 'Hello', '2022-05-03 09:41:14', 1),
+(26, 83, 68, 'Hello', '2022-05-03 09:51:28', 1);
 
 -- --------------------------------------------------------
 
@@ -72,9 +74,9 @@ CREATE TABLE `login_details` (
 --
 
 INSERT INTO `login_details` (`login_details_id`, `user_id`, `last_activity`, `is_type`) VALUES
-(97, 84, '2022-05-02 03:37:28', 'no'),
-(98, 83, '2022-05-02 03:43:44', 'no'),
-(99, 68, '2022-05-02 03:45:35', 'no');
+(127, 68, '2022-05-03 10:50:09', 'no'),
+(128, 114, '2022-05-03 10:51:50', 'no'),
+(129, 68, '2022-05-03 10:51:57', 'no');
 
 -- --------------------------------------------------------
 
@@ -83,7 +85,12 @@ INSERT INTO `login_details` (`login_details_id`, `user_id`, `last_activity`, `is
 --
 
 CREATE TABLE `notifications` (
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `notif_message` text NOT NULL,
+  `datetime` varchar(255) NOT NULL,
+  `seen_status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -122,7 +129,7 @@ CREATE TABLE `personal` (
 INSERT INTO `personal` (`personal_id`, `user_id`, `middle_name`, `category`, `address`, `dob`, `mobile`, `card_number`, `mother_name`, `father_name`, `spouse_name`, `contact_person`, `contact_number`, `s_contact_number`, `company_affiliated`, `company_address`, `company_number`, `position`, `work_status`, `image`) VALUES
 (48, 68, '', '0', '', '', '09206551000', '2022-0068', '', '', '', '', '', '', '', '', '', '', '', '2022-04-28 03-17-24.Leana.png'),
 (63, 83, '', '0', '', '', '', '2022-0083', '', '', '', '', '', '', '', '', '', '', '', '2022-05-01 12-48-17.jordi.jpg'),
-(64, 84, '', '0', '', '', '', '2022-0084', '', '', '', '', '', '', '', '', '', '', '', '');
+(95, 115, '', '', '', '', '', '2022-0115', '', '', '', '', '', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -149,7 +156,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `email`, `password`, `usertype`, `status`, `signup_date`) VALUES
 (68, 'Admin', 'Account', 'admin_account', 'admin@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Admin', 'Verified', '2022-04-28'),
 (83, 'Branch', 'Account', 'peter_parker', 'branch@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Branch Manager', 'Verified', '2022-04-28'),
-(84, 'Staff', 'Account', 'carl_caraan', 'staff@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Staff', 'Verified', '2022-04-28');
+(115, 'Staff', 'Account', 'staff_account', 'staff@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Staff', 'Verified', '2022-05-03');
 
 --
 -- Indexes for dumped tables
@@ -199,7 +206,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `chat`
 --
 ALTER TABLE `chat`
-  MODIFY `chat_message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `chat_message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `loans`
@@ -211,25 +218,25 @@ ALTER TABLE `loans`
 -- AUTO_INCREMENT for table `login_details`
 --
 ALTER TABLE `login_details`
-  MODIFY `login_details_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+  MODIFY `login_details_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
 
 --
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `personal`
 --
 ALTER TABLE `personal`
-  MODIFY `personal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+  MODIFY `personal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
