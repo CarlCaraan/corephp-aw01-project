f9f454f95c0e793994e1c2d8f158e627da8fc8f8
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-05-04 11:01:34 +0800

    5/44/22

--- loaning.sql
+++ loaning.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2022 at 12:53 PM
+-- Generation Time: May 04, 2022 at 05:00 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -24,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `category` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `category`) VALUES
+(1, 'ATM'),
+(2, 'SPS'),
+(3, 'SPSV1');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `chat`
 --
 
@@ -31,7 +51,7 @@ CREATE TABLE `chat` (
   `chat_message_id` int(11) NOT NULL,
   `to_user_id` int(11) NOT NULL,
   `from_user_id` int(11) NOT NULL,
-  `chat_message` text NOT NULL,
+  `chat_message` text DEFAULT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
   `status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -44,7 +64,9 @@ INSERT INTO `chat` (`chat_message_id`, `to_user_id`, `from_user_id`, `chat_messa
 (22, 83, 68, 'test', '2022-05-01 12:38:00', 1),
 (23, 83, 84, 'I have a concern with my account', '2022-05-01 14:27:33', 1),
 (25, 84, 68, 'Hello', '2022-05-03 09:41:14', 1),
-(26, 83, 68, 'Hello', '2022-05-03 09:51:28', 1);
+(26, 83, 68, 'Hello', '2022-05-03 09:51:28', 1),
+(29, 117, 68, 'hi', '2022-05-04 00:42:48', 2),
+(30, 0, 68, '<p class=\"mt-5\"><img src=\"../resources/img/uploads/yangyi-shi-ee-student-2-compressed-Cropped-293x228.jpg\" class=\"img-thumbnail\" width=\"200\" height=\"160\"></p><br>', '2022-05-04 02:07:22', 1);
 
 -- --------------------------------------------------------
 
@@ -74,9 +96,9 @@ CREATE TABLE `login_details` (
 --
 
 INSERT INTO `login_details` (`login_details_id`, `user_id`, `last_activity`, `is_type`) VALUES
-(127, 68, '2022-05-03 10:50:09', 'no'),
-(128, 114, '2022-05-03 10:51:50', 'no'),
-(129, 68, '2022-05-03 10:51:57', 'no');
+(147, 115, '2022-05-04 01:54:11', 'no'),
+(148, 68, '2022-05-04 02:00:24', 'no'),
+(149, 68, '2022-05-04 02:57:36', 'no');
 
 -- --------------------------------------------------------
 
@@ -93,6 +115,13 @@ CREATE TABLE `notifications` (
   `seen_status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `fname`, `lname`, `notif_message`, `datetime`, `seen_status`) VALUES
+(16, 'Peter', 'Parker', 'has account pending confirmation', '2022-05-03 21:54:14', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -103,20 +132,20 @@ CREATE TABLE `personal` (
   `personal_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `middle_name` varchar(255) DEFAULT NULL,
-  `category` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `dob` varchar(255) NOT NULL DEFAULT 'Not Set',
+  `category_id` varchar(255) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `dob` varchar(255) DEFAULT 'Not Set',
   `mobile` varchar(255) DEFAULT NULL,
   `card_number` varchar(255) DEFAULT NULL,
-  `mother_name` varchar(255) NOT NULL,
-  `father_name` varchar(255) NOT NULL,
+  `mother_name` varchar(255) DEFAULT NULL,
+  `father_name` varchar(255) DEFAULT NULL,
   `spouse_name` varchar(255) DEFAULT NULL,
-  `contact_person` varchar(255) NOT NULL,
-  `contact_number` varchar(255) NOT NULL,
+  `contact_person` varchar(255) DEFAULT NULL,
+  `contact_number` varchar(255) DEFAULT NULL,
   `s_contact_number` varchar(255) DEFAULT NULL,
-  `company_affiliated` varchar(255) NOT NULL,
-  `company_address` varchar(255) NOT NULL,
-  `company_number` varchar(255) NOT NULL,
+  `company_affiliated` varchar(255) DEFAULT NULL,
+  `company_address` varchar(255) DEFAULT NULL,
+  `company_number` varchar(255) DEFAULT NULL,
   `position` varchar(255) DEFAULT NULL,
   `work_status` varchar(255) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL
@@ -126,10 +155,10 @@ CREATE TABLE `personal` (
 -- Dumping data for table `personal`
 --
 
-INSERT INTO `personal` (`personal_id`, `user_id`, `middle_name`, `category`, `address`, `dob`, `mobile`, `card_number`, `mother_name`, `father_name`, `spouse_name`, `contact_person`, `contact_number`, `s_contact_number`, `company_affiliated`, `company_address`, `company_number`, `position`, `work_status`, `image`) VALUES
-(48, 68, '', '0', '', '', '09206551000', '2022-0068', '', '', '', '', '', '', '', '', '', '', '', '2022-04-28 03-17-24.Leana.png'),
-(63, 83, '', '0', '', '', '', '2022-0083', '', '', '', '', '', '', '', '', '', '', '', '2022-05-01 12-48-17.jordi.jpg'),
-(95, 115, '', '', '', '', '', '2022-0115', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO `personal` (`personal_id`, `user_id`, `middle_name`, `category_id`, `address`, `dob`, `mobile`, `card_number`, `mother_name`, `father_name`, `spouse_name`, `contact_person`, `contact_number`, `s_contact_number`, `company_affiliated`, `company_address`, `company_number`, `position`, `work_status`, `image`) VALUES
+(48, 68, '', '2', '', '', '09559168806', '2022-0068', '', '', '', '', '', '', '', '', '', '', '', '2022-05-04 10-35-24.student-profile-javon-nathaniel.jpg'),
+(63, 83, '', '2', '', '', '', '2022-0083', '', '', '', '', '', '', '', '', '', '', '', '2022-05-03 10-06-50.jordi.jpg'),
+(95, 115, '', '1', '', '', '', '2022-0115', '', '', '', '', '', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -139,14 +168,14 @@ INSERT INTO `personal` (`personal_id`, `user_id`, `middle_name`, `category`, `ad
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   `usertype` varchar(255) DEFAULT NULL,
   `status` varchar(255) NOT NULL DEFAULT 'Pending',
-  `signup_date` varchar(255) NOT NULL
+  `signup_date` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -161,6 +190,12 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `email`, `pass
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `chat`
@@ -203,10 +238,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `chat`
 --
 ALTER TABLE `chat`
-  MODIFY `chat_message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `chat_message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `loans`
@@ -218,25 +259,25 @@ ALTER TABLE `loans`
 -- AUTO_INCREMENT for table `login_details`
 --
 ALTER TABLE `login_details`
-  MODIFY `login_details_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
+  MODIFY `login_details_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=150;
 
 --
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `personal`
 --
 ALTER TABLE `personal`
-  MODIFY `personal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+  MODIFY `personal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
